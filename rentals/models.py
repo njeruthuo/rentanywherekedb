6d36fe98142969ce14aedfc46b55e1806604fe54
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-01-27 08:33:05 +0300

    ok change

--- rentals/models.py
+++ rentals/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 
+
 class RentalTypeChoices(models.TextChoices):
     SINGLE = 'SINGLE ROOM', 'SR'
     BEDSITTER = 'BEDSITTER', 'BS'
@@ -11,11 +12,13 @@ class RentalTypeChoices(models.TextChoices):
     OFFICE = 'OFFICE SPACE', 'OFFICE'
     STALL = 'BUSINESS STALL', 'STALL'
 
+
 class Rental(models.Model):
     building_name = models.CharField(max_length=200, blank=True)
     room_size = models.JSONField()  # dimensions of the rooms
     location = models.JSONField()  # coordinates to be added from Google Maps.
-    rental_type = models.CharField(max_length=20, choices=RentalTypeChoices.choices, default=RentalTypeChoices.BEDSITTER)
+    rental_type = models.CharField(
+        max_length=20, choices=RentalTypeChoices.choices, default=RentalTypeChoices.BEDSITTER)
     distance_from_tarmac = models.CharField(max_length=100)
     distance_from_cbd = models.CharField(max_length=200)
     pricing = models.DecimalField(max_digits=10, decimal_places=2)
@@ -30,9 +33,11 @@ class Rental(models.Model):
     description = models.TextField()  # add info about the rental house.
     date_created = models.DateTimeField(auto_now_add=True)
 
-class RentalImage(models.Model):
-    rental = models.ForeignKey(Rental, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='rental_images/')
-    caption = models.CharField(max_length=255, blank=True, null=True)  # Optional caption for the image
-    date_uploaded = models.DateTimeField(auto_now_add=True)
 
+class RentalImage(models.Model):
+    rental = models.ForeignKey(
+        Rental, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='rental_images/')
+    # Optional caption for the image
+    caption = models.CharField(max_length=255, blank=True, null=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
